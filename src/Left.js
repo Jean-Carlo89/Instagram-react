25c22f609b8a0ed357cd4cbace34d8c4a3897e6e
Jean Carlo <jeancarlomo@hotmail.com>
2021-04-30 11:02:55 -0300

    feed

--- src/Left.js
+++ src/Left.js
@@ -1,10 +1,11 @@
 import Stories from './Stories'
+import Feed from './Feed'
 
 export default function Left(){
     return(
         <div class="esquerda">
             <Stories/>
-
+            <Feed/>
          
         </div>
     )
--- src/Stories.js
+++ src/Stories.js
@@ -1,81 +1,36 @@
+import StoriesToJSX from './TransformToJSX'
+
+
+
 export default function Stories(){
     return(
-        <div class="stories">
-            <div class="story">
+       <div class="stories">
+        {StoriesToJSX()}
+        <div class="setinha">
+        <ion-icon name="chevron-forward-circle"></ion-icon>
+        </div>
+       </div>
+
+    )
+}
+
+
+
+
+
+/* tentativa 1 incompeleta
+
+function Story(){
+    <div class="story">
               <div class="imagem">
-                <img src="assets/img/9gag.svg" />
+                stories.
               </div>
+             
               <div class="usuario">
                 9gag
               </div>
-            </div>
+    </div>
+}                
 
-            <div class="story">
-              <div class="imagem">
-                <img src="assets/img/meowed.svg" />
-              </div>
-              <div class="usuario">
-                meowed
-              </div>
-            </div>
+*/
 
-            <div class="story">
-              <div class="imagem">
-                <img src="assets/img/barked.svg" />
-              </div>
-              <div class="usuario">
-                barked
-              </div>
-            </div>
-
-            <div class="story">
-              <div class="imagem">
-                <img src="assets/img/nathanwpylestrangeplanet.svg" />
-              </div>
-              <div class="usuario">
-                nathanwpylestrangeplanet
-              </div>
-            </div>
-
-            <div class="story">
-              <div class="imagem">
-                <img src="assets/img/wawawicomics.svg" />
-              </div>
-              <div class="usuario">
-                wawawicomics
-              </div>
-            </div>
-            
-            <div class="story">
-              <div class="imagem">
-                <img src="assets/img/respondeai.svg" />
-              </div>
-              <div class="usuario">
-                respondeai
-              </div>
-            </div>
-
-            <div class="story">
-              <div class="imagem">
-                <img src="assets/img/filomoderna.svg" />
-              </div>
-              <div class="usuario">
-                filomoderna
-              </div>
-            </div>
-
-            <div class="story">
-              <div class="imagem">
-                <img src="assets/img/memeriagourmet.svg" />
-              </div>
-              <div class="usuario">
-                memeriagourmet
-              </div>
-            </div>
-
-            <div class="setinha">
-              <ion-icon name="chevron-forward-circle"></ion-icon>
-            </div>
-          </div>
-    )
-}
